6c3a0cacd31fb93d5804cb5ca0a7b0e0bfdabd96
chilam <chilam.Lee@outlook.com>
2022-09-13 23:23:16 +0800

    :sparkles: feat: add basic appearance

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,42 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import { invoke } from "@tauri-apps/api/tauri";
-import "./App.css";
 import { appWindow } from "@tauri-apps/api/window";
+import styled from "@emotion/styled";
 
 function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
-
-  async function greet() {
-    setGreetMsg(await invoke("greet", { name }));
-  }
-
   return (
-    <input
-      id="greet-input"
-      onChange={(e) => setName(e.currentTarget.value)}
-      placeholder="Enter a name..."
-    />
+    <Container>
+      <SearchBox id="greet-input" placeholder="Brisk..." />
+    </Container>
   );
 }
+
+const Container = styled.div`
+  margin: 0;
+  padding: 0;
+  display: flex;
+  height: 100%;
+  width: 100%;
+  justify-content: center;
+  align-items: center;
+`;
+const SearchBox = styled.input`
+  box-sizing: border-box;
+  width: 100%;
+  height: 100%;
+  margin: 0;
+  padding: 0;
+  border-radius: 10px;
+  outline: none;
+  border: 1px dashed black;
+  padding: 0.6em 1.2em;
+  font-size: 1.2em;
+  font-weight: 500;
+  font-family: inherit;
+  color: #0f0f0f;
+  background-color: #ffffff;
+  transition: border-color 0.25s;
+`;
 
 export default App;
